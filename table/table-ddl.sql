b9e19494599329d8f0d66a7a294af668ae3dd162
dba-genesis <91065213+dba-genesis@users.noreply.github.com>
2021-09-21 13:57:22 +0530

    Update table-ddl.sql

--- table/table-ddl.sql
+++ table/table-ddl.sql
@@ -7,7 +7,18 @@ set feedback off ;
 set verify off ;
 set trimspool on;
 
+/* this will only print the create table with respective tablespace details only */
+BEGIN
+DBMS_METADATA.set_transform_param (DBMS_METADATA.session_transform, 'SQLTERMINATOR', true);
+DBMS_METADATA.set_transform_param (DBMS_METADATA.session_transform, 'PRETTY', true);
+DBMS_METADATA.set_transform_param (DBMS_METADATA.session_transform, 'SEGMENT_ATTRIBUTES', true);
+DBMS_METADATA.set_transform_param (DBMS_METADATA.session_transform, 'STORAGE', false);
+END;
+/
+
+/* ask user to input table name and owner */
 ACCEPT table_name CHAR PROMPT 'Enter the table name > '
 ACCEPT schema_name CHAR PROMPT 'Enter table schema (owner) name > '
 
+/* print the DDL command */
 SELECT DBMS_METADATA.get_ddl ('TABLE', upper('&table_name'),upper('&schema_name')) from dual;
